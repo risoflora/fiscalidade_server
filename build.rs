8f89acc7d107478f314eea3312f9d8f0a6b88500
silvioprog <silvioprog@gmail.com>
2020-02-28 09:03:26 -0300

    Linka recursos apenas em build para release.

--- build.rs
+++ build.rs
@@ -4,14 +4,18 @@ use winres;
 #[cfg(target_os = "windows")]
 fn main() {
     use std::io::Write;
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("resources\\ico\\fiscalidade_server.ico");
-    match res.compile() {
-        Err(e) => {
-            write!(std::io::stderr(), "{}", e).unwrap();
-            std::process::exit(1);
+    // only build the resource for release builds
+    // as calling rc.exe might be slow
+    if std::env::var("PROFILE").unwrap() == "release" {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("resources\\ico\\fiscalidade_server.ico");
+        match res.compile() {
+            Err(error) => {
+                write!(std::io::stderr(), "{}", error).unwrap();
+                std::process::exit(1);
+            }
+            Ok(_) => {}
         }
-        Ok(_) => {}
     }
 }
 
