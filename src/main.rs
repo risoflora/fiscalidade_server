1520877c341941a6d97467073ecbb966ee8a991c
silvioprog <silvioprog@gmail.com>
2020-02-28 14:29:25 -0300

    Permite que a aplicação rode em modo de debug.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+#[cfg(not(debug_assertions))]
 #[cfg(any(target_os = "linux"))]
 #[path = "daemon_linux.rs"]
-mod service;
+mod server;
 
+#[cfg(not(debug_assertions))]
 #[cfg(target_os = "windows")]
 #[path = "service_windows.rs"]
-mod service;
+mod server;
+
+#[cfg(debug_assertions)]
+use fiscalidade_server as server;
 
 fn main() -> anyhow::Result<()> {
-    service::run()
+    server::run()
 }
