e0a4e1b7a75ed0e172c48f88282d3bc2b4c15e6d
silvioprog <silvioprog@gmail.com>
2020-02-28 09:42:41 -0300

    Força executável a solicitar direitos de administrador no Windows.

--- build.rs
+++ build.rs
@@ -8,7 +8,20 @@ fn main() {
     // as calling rc.exe might be slow
     if std::env::var("PROFILE").unwrap() == "release" {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("resources\\ico\\fiscalidade_server.ico");
+        res.set_icon("resources\\ico\\fiscalidade_server.ico")
+            .set_manifest(
+                r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+</trustInfo>
+</assembly>
+"#,
+            );
         match res.compile() {
             Err(error) => {
                 write!(std::io::stderr(), "{}", error).unwrap();
